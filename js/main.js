577ceb02789efca687e226b6cea63f4ecd115097
Roman-ua <60047390+Roman-ua@users.noreply.github.com>
2020-06-08 22:55:37 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+
+    $(".header__nav-gamburger").click(function() {
+        $(".header__nav-items").toggleClass("active__menu"),
+        $("body").toggleClass("active__body");         
+        $(this).toggleClass("active");
+    });
+
+    $(".form__button").click(function() {
+        $(".form__thank").toggleClass("active__text"),        
+        $(this).toggleClass("active__text");
+    });
+    
+});
